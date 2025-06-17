b285b1781ebb70c53c4bfbf0de778fdd5a1d1bc5
BadLuckZ <thanagorn8802@gmail.com>
2025-06-17 23:05:48 +0700

    Connect Postgres

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-    app := fiber.New()
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
-    app.Listen(":3000")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Load .env Error!")
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"))
+
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection Successful", db)
+
+	db.AutoMigrate(&User{}) // Automatically create User Table...
+
+	fmt.Println("Create/Update User Table Successful")
 }
---
+++ user.go
@@ -0,0 +1,11 @@
+package main
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
